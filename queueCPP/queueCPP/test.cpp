127dc44d65561c6ff64baae30ffdd6255eb96a38
曹锋 <1069335293@qq.com>
2020-08-05 00:47:25 +0800

    更新代码

--- queueCPP/queueCPP/test.cpp
+++ queueCPP/queueCPP/test.cpp
@@ -2,13 +2,12 @@
 #include<list>
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 namespace xff {
-	template<class T>
+	template<class T,class Container>
 	class queue {
 	public:
-		queue(){}
-		~queue(){}
 		void push(const T&val)  { _q.push_back(val);  }
 		void pop()				{ _q.pop_front();     }
 		T& front()				{ return  _q.front(); }
@@ -18,10 +17,11 @@ namespace xff {
 		bool empty()			{ return _q.empty();  }
 		const size_t size()const{ return _q.size();   }
 	private:
-		list<T> _q;
+		Container _q;
 	};
 	void mytest() {
-		queue<string> q;
+		queue<string,list<string>> q;
+		//queue<string, vector<string>> q;//vector中没有pop_front()接口
 		q.push("hello");
 		q.push("right");
 		q.push("string");
@@ -50,9 +50,55 @@ void sys_test() {
 	cout << endl;
 	cout << q.empty() << " " << q.size() << endl;
 }
+void deque_test()
+{
+	deque<int> d;
+	d.push_back(2);
+	d.push_front(1);
+	d.push_back(3);
+	d.push_back(5);
+	cout << d.front() << " " << d.back() << endl;
+	deque<int>::iterator pos = find(d.begin(),d.end(),3);
+	d.insert(pos, 5);
+	for (auto&e : d)
+	{
+		cout << e << " ";
+	}
+	cout << endl;
+}
+
+#include<time.h>
+void test()
+{
+	deque<int> d;
+	vector<int> v;
+	const int n = 100000;
+	int* a1 = new int[n];
+	int* a2 = new int[n];
+	srand(time(0));
+	for (int i = 0; i < n; ++i)
+	{
+		int val = rand();
+		d.push_back(val);
+		v.push_back(val);
+	}
+	size_t begin1 = clock();
+	sort(d.begin(), d.end());
+	size_t end1 = clock(); 
+
+	size_t begin2 = clock();
+	sort(v.begin(), v.end());
+	size_t end2 = clock();
+
+	cout <<"deque->"<<end1 - begin1 << endl;
+	cout << "vector->" << end2 - begin2 << endl;
+}
 int main() {
 	//sys_test();
-	xff::mytest();
+	//xff::mytest();
+	//deque_test();
+	test();
 	system("pause");
 	return 0;
 }
+
