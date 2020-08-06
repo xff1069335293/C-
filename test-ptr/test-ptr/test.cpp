82fb91db5415710ee92636a3d8c7c2f7717a77f1
曹锋 <1069335293@qq.com>
2020-08-06 17:52:12 +0800

    提交代码

--- test-ptr/test-ptr/test.cpp
+++ test-ptr/test-ptr/test.cpp
@@ -1,5 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+#include<thread>
+#include<mutex>
 using namespace std;
 namespace xff
 {
@@ -9,7 +11,8 @@ namespace xff
 	public:
 		Smartptr(T* ptr)
 			:_ptr(ptr)
-			,_p(new int(1))
+			, _pcount(new int(1))
+			,_pmutex(new mutex)
 		{
 			cout << "Smartptr()" << endl;
 		}
@@ -29,13 +32,14 @@ namespace xff
 		//shared_ptr
 		~Smartptr()
 		{
-			if (--*_p == 0 && _ptr)
+			/*if (--*_pcount == 0 && _ptr)
 			{
 				delete _ptr;
 				cout << "~Smartptr():" << _ptr << endl;
 				_ptr = nullptr;
-				_p = nullptr;
-			}	
+				_pcount = nullptr;
+			}	*/
+			release();
 		}
 		T* operator->()
 		{
@@ -45,7 +49,10 @@ namespace xff
 		{
 			return *_ptr;
 		}
-
+		T* get_ptr()
+		{
+			return _ptr;
+		}
 		//auto_ptr 
 		//1.管理权转移  C++98 
 		/*Smartptr(Smartptr<T>& tmp)
@@ -78,32 +85,67 @@ namespace xff
 		{
 			if (this != &tmp)
 			{
-				if (--(*_p) == 0)//如果当前对象还管理着其他资源，要判断去除本次计数之后的数据，考虑释放问题。
+				/*if (--(*_pcount) == 0)//如果当前对象还管理着其他资源，要判断去除本次计数之后的数据，考虑释放问题。
 				{
 					delete _ptr;
-					delete _p;
-					_p = _ptr = nullptr;
-				}
+					delete _pcount;
+					_pcount = _ptr = nullptr;
+					cout << "~Smartptr():" << _ptr << endl;
+				}*/
+				release();
 				_ptr = tmp._ptr;
-				_p = tmp._p;
-				++*_p;
+				_pcount = tmp._pcount;
+				_pmutex = tmp._pmutex;
+
+				add_ref_count();
 			}
 			return *this;
 		}
 		Smartptr(Smartptr<T>& tmp)
 			:_ptr(tmp._ptr)
-			,_p(tmp._p)	
+			, _pcount(tmp._pcount)
+			,_pmutex(tmp._pmutex)
 		{
-			++*_p;
+			add_ref_count();
 		}
-		
+		int use_count()
+		{
+			return *_pcount;
+		}
+		void add_ref_count()
+		{
+			_pmutex->lock();
+			++(*_pcount); 
+			_pmutex->unlock();
+		}
+		void  release()
+		{
+			bool flag = false;
+			_pmutex->lock();
 
-	
+			if (--(*_pcount) == 0)
+			{
+				cout << "~Smartptr():" << _ptr << endl;
+				delete _ptr;
+				_ptr = nullptr;
+
+				delete _pcount;
+				_pcount = nullptr;
+				flag = true;
+			}
+			_pmutex->unlock();
+			if (flag == true)
+			{
+				delete _pmutex;
+				_pmutex = nullptr;
+			}
+		}
 	private:
 		T* _ptr;
 		//static int _count;
 		//用静态变量的话，同类型多个对象就不能区分了
-		int* _p;
+		int* _pcount;
+		mutex* _pmutex;
 	};
 	//template<class T>
 	//int Smartptr<T>::_count = 1;
@@ -153,6 +195,30 @@ namespace xff
 		Smartptr<int> pi5(new int);
 		Smartptr<int> pi6(pi5);
 	}
+
+	void test_thread()
+	{
+		Smartptr<int> sp(new int);
+		cout << sp.use_count() << endl;
+		int n = 100000;
+		thread t1([&]() {
+			for (int i = 0; i < n; ++i)
+			{ 
+				Smartptr<int> sp1(sp);
+			}
+		});
+
+		thread t2([&]() {
+			for (int i = 0; i < n; ++i)
+			{
+				Smartptr<int> sp2(sp);
+			}	
+		});
+
+		t1.join();
+		t2.join();
+		cout << sp.use_count() << endl;
+	}
 }
 
 
@@ -185,6 +251,97 @@ void test_shared_ptr()
 	shared_ptr<int> pi5(new int);
 	shared_ptr<int> pi6(pi5);
 }
+
+class A
+{
+public:
+
+	~A()
+	{
+		cout << "~A()" << endl;
+	}
+private:
+	int _a;
+};
+
+template<class T>//定制删除器
+struct deleteArray
+{
+	void operator()(T* pa)
+	{
+		delete[] pa;
+		pa = nullptr;
+	}
+};
+void test_A()
+{
+	shared_ptr<A> sp1(new A);
+	shared_ptr<A> sp2(new A[10],deleteArray<A>());
+}
+
+template<class T>
+class Weak_ptr
+{
+public:
+	Weak_ptr() 
+	{
+		cout << "Weak_ptr()" << endl;
+	}
+	Weak_ptr(const xff::Smartptr<T>& sp)
+		:_ptr(sp.get_ptr())
+	{
+		
+	}
+	Weak_ptr<T>&operator=(xff::Smartptr<T>& sp)
+	{
+		_ptr = sp.get_ptr();
+		return *this;
+	}
+	~Weak_ptr()
+	{
+		cout << "~Weak_ptr()" << endl;
+	}
+	T* operator->()
+	{
+		return _ptr;
+	}
+	T& operator*()
+	{
+		return *_ptr;
+	}
+private:
+	T* _ptr;
+};
+class ListNode
+{
+public:
+	ListNode()
+	{
+		cout << "ListNode()" << endl;
+	}
+
+	~ListNode()
+	{
+		cout << "~ListNode()" << endl;
+	}
+	int _val;
+	//shared_ptr<ListNode> _spnext;
+	//shared_ptr<ListNode> _spprev;
+
+	Weak_ptr<ListNode> _spnext;
+	Weak_ptr<ListNode> _spprev;
+};
+
+void test_ListNode()//循环引用问题
+{
+	xff::Smartptr<ListNode> sp1(new ListNode);
+	xff::Smartptr<ListNode> sp2(new ListNode);
+	
+	sp1->_spnext = sp2;
+	sp2->_spprev = sp1;
+	
+}
+
 int main()
 {
 	//xff::test_Smartptr();
@@ -197,7 +354,11 @@ int main()
 	//test_shared_ptr();
 	//xff::test_shared_ptr();
 
-	
+	//xff::test_thread();//线程安全问题
+	//test_ListNode();//weak_ptr
+
+	test_A();
+
 
 
 	system("pause");
